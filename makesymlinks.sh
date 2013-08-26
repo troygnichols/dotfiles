638fd52309db38aedaebf21f6dc1a84462e3181e
Troy Nichols <tnichols@onsitehd.com>
2013-08-26 18:43:34 -0500

    Clean up symlink script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+dotfiles=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+backup=$HOME/.dotfiles_backup
+files="bashrc vimrc vim zshrc oh-my-zsh"
 
-########## Variables
+echo "Creating backup directory: $backup"
+mkdir -p $backup
 
-dir=`pwd`                    			# dotfiles directory
-olddir=~/dotfiles_old             		# old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh"    	# list of files/folders to symlink in homedir
+cd $dotfiles
 
-##########
-
-# create dotfiles_old in homedir
-
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+  if [ -e "$HOME/.$file" ]; then
+    if [ -h "$HOME/.$file" ]; then
+      echo "Deleting old symlink: $HOME/.$file"
+      rm "$HOME/.$file"
+    else
+      echo "Backing up $file"
+      mv $HOME/.$file $backup
+    fi
+  fi
+  echo "Linking $file"
+  ln -vsfF $dotfiles/$file $HOME/.$file
 done
 
 
