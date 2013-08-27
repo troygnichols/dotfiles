97ca62a5e2d85fab7930a25f1561be084299624e
Troy Nichols <tnichols@onsitehd.com>
2013-08-26 19:05:45 -0500

    Add gitconfig and gitignore

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dotfiles=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 backup=$HOME/.dotfiles_backup
-files="bashrc vimrc vim zshrc oh-my-zsh"
+files="bashrc vimrc vim zshrc oh-my-zsh gitconfig gitignore"
 
 echo "Creating backup directory: $backup"
 mkdir -p $backup
