8ff854d74a37ec65b41e0e39473961967780a261
Troy Nichols <troygnichols@gmail.com>
2019-08-19 21:08:45 -0500

    Refactor install script
    - Break out into functions
    - Add VERBOSE mode, off by default

--- install.sh
+++ install.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
 
+## set VERBOSE for more output, .e.g run the script this way:
+## VERBOSE=yes ./install.sh
+
 dotfiles=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 backup=$HOME/.dotfiles_backup
-files="bin bashrc aliases zshrc zsh-nocorrect oh-my-zsh oh-my-zsh-custom vimrc gvimrc vim xvimrc gitconfig gitignore gemrc tmux.conf pythonrc irbrc pryrc curl-format user_scripts screenrc config/nvim/init.vim"
+files=(bin bashrc aliases zshrc zsh-nocorrect oh-my-zsh oh-my-zsh-custom vimrc gvimrc vim gitconfig gitignore gemrc tmux.conf pythonrc irbrc pryrc curl-format user_scripts screenrc config/nvim/init.vim)
 
-echo "Creating backup directory: $backup"
+echo "*** Creating backup directory: $backup"
 mkdir -p "$backup"
 
-echo "Changing directory to: $dotfiles"
-cd $dotfiles
+echo "*** Changing directory to: $dotfiles" cd $dotfiles
 
-for file in $files; do
-  if [ -e "$HOME/.$file" ]; then
-    if [ -h "$HOME/.$file" ]; then
-      echo "Deleting old symlink: $HOME/.$file"
-      rm "$HOME/.$file"
-    else
-      echo "Backing up $file"
-      mv $HOME/.$file $backup
-    fi
-  fi
-  filepath="$HOME/.$file"
-  parent_dir="${filepath%/*}"
+# create the config file's parent dir if needed
+make_parent_dir() {
+  local filepath="$1"
+  local parent_dir="${filepath%/*}"
   if ! [[ -d "$parent_dir" ]]; then
-    echo "*** Creating parent directory: $parent_dir, for file: $file"
+    [[ -n "$VERBOSE" ]] && echo "*** Creating parent directory: $parent_dir, for file: $filename"
     mkdir -p "$parent_dir"
   fi
-  echo "==> Linking $file"
-  ln -vsfF "$dotfiles/$file" "$filepath"
+}
+
+# copy and rename the config file appropriately
+copy_file() {
+  local filename="$1"
+  local filepath="$HOME/.$filename"
+
+  if [ -e "$filepath" ]; then
+    if [ -h "$filepath" ]; then
+      [[ -n "$VERBOSE" ]] && echo "*** Deleting old symlink: $filepath"
+      rm "$filepath"
+    else
+      [[ -n "$VERBOSE" ]] && echo "*** Backing up $filename"
+      mv "$filepath" "$backup"
+    fi
+  fi
+
+  make_parent_dir "$filepath"
+  echo "==> Linking $filepath"
+
+  local args=(sfF)
+  [[ -n "$VERBOSE" ]] && args+="v"
+  ln -"$args" "$dotfiles/$filename" "$filepath"
+}
+
+if [[ "$1" -eq "--help" ]]; then
+  echo "help"
+  exit 0
+fi
+
+for filename in "${files[@]}"; do
+  copy_file "$filename"
 done
