51e92f1f71d7de59d0527b8a9cf4636ead7f6360
Troy Nichols <troygnichols@gmail.com>
2019-08-19 20:14:20 -0500

    Remove unused neovim stuff

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 dotfiles=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 backup=$HOME/.dotfiles_backup
 files="bin bashrc aliases zshrc zsh-nocorrect oh-my-zsh oh-my-zsh-custom vimrc gvimrc vim xvimrc gitconfig gitignore gemrc tmux.conf pythonrc irbrc pryrc curl-format user_scripts screenrc config/nvim/init.vim"
-scripts="neovim.sh"
 
 echo "Creating backup directory: $backup"
 mkdir -p "$backup"
@@ -29,9 +28,4 @@ for file in $files; do
   fi
   echo "==> Linking $file"
   ln -vsfF "$dotfiles/$file" "$filepath"
-done
-
-for script in $scripts; do
-  echo "Running script $script"
-
 done
--- neovim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Linking UltiSnips for neovim"
-ln -s $HOME/.vim/UltiSnips $HOME/.config/nvim/Ultisnips
-
